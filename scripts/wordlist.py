1c12174ac3d24e286a24857c2bba90d4551015ce
Open English Bible <oebible@openenglishbible.org>
2011-03-20 21:45:13 +1100

    Various additions and fixes

--- scripts/wordlist.py
+++ scripts/wordlist.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 import re
+
+def bylength(word1, word2):
+    """
+    write your own compare function:
+    returns value < 0 of word1 longer then word2
+    returns value = 0 if the same length
+    returns value > 0 of word2 longer than word1
+    """
+    return len(word2) - len(word1)
  
-f = open('../../USFM-Tools/built/Bible.txt')
+f = open('../../USFM-Tools/built/OEB-NT-Current.txt')
 fc = unicode(f.read(), 'utf-8')
 f.close()
  
@@ -24,6 +33,8 @@ noDupes=[]
 #filtered=[]
 #[filtered.append(i) for i in noDupes if i[-3:] == u'eth' or i[-3:] == u'est']
 filtered = noDupes
+
+filtered.sort(cmp=bylength)
 
 result = u''
 for n in filtered: result = result + u'\n' + n
